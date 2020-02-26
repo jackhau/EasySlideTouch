f0217673a59a02b0b5ad7f7b7ce0c9c390eb82c3
TAN JACK HAU <jackhau@agmostudio.com>
2020-02-26 15:36:10 +0800

    init project

---
+++ app/src/main/java/com/example/slidetouch/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.slidetouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.easyslidetouch.EasySlideTouch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ easyslidetouch/src/main/java/com/example/easyslidetouch/EasySlideTouch.java
@@ -0,0 +1,21 @@
+package com.example.easyslidetouch;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class EasySlideTouch extends ConstraintLayout {
+
+    public EasySlideTouch(Context context) {
+        super(context);
+    }
+
+    public EasySlideTouch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EasySlideTouch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
