610344e906cb9e4be85c4b78c2042a8f33371c2a
TAN JACK HAU <jackhau@agmostudio.com>
2020-02-26 19:24:17 +0800

    add library EasySlideTouch

--- easyslidetouch/src/main/java/com/example/easyslidetouch/EasySlideTouch.java
+++ easyslidetouch/src/main/java/com/example/easyslidetouch/EasySlideTouch.java
@@ -2,6 +2,7 @@ package com.example.easyslidetouch;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -13,9 +14,21 @@ public class EasySlideTouch extends ConstraintLayout {
 
     public EasySlideTouch(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public EasySlideTouch(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
     }
+
+    private void init(Context context, AttributeSet attrs) {
+        LayoutInflater.from(getContext()).inflate(R.layout.slide_touch, this, true);
+
+//        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlideTouch,
+//                0, 0);
+
+    }
+
+
 }
