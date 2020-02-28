f45d8c98104573b37bc80f33fd50104fc570281c
TAN JACK HAU <jackhau@agmostudio.com>
2020-02-28 14:40:20 +0800

    after slide set back to 0 position

--- easyslidetouch/src/main/java/com/example/easyslidetouch/EasySlideTouch.java
+++ easyslidetouch/src/main/java/com/example/easyslidetouch/EasySlideTouch.java
@@ -5,6 +5,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.ViewCompat;
@@ -14,6 +16,9 @@ import androidx.lifecycle.Observer;
 public class EasySlideTouch extends ConstraintLayout {
 
     private MutableLiveData<Integer> count;
+    private TextView counterTv;
+    private FrameLayout counterFl;
+    private Float start = 0F;
 
     public EasySlideTouch(Context context) {
         super(context);
@@ -32,6 +37,9 @@ public class EasySlideTouch extends ConstraintLayout {
     private void init(Context context, AttributeSet attrs) {
         LayoutInflater.from(getContext()).inflate(R.layout.slide_touch, this, true);
 
+        counterTv = findViewById(R.id.viewCounterText);
+        counterFl= findViewById(R.id.viewCounter);
+
         count = new MutableLiveData<>();
         count.setValue(0);
         count.observeForever(dd);
@@ -43,35 +51,40 @@ public class EasySlideTouch extends ConstraintLayout {
 
     private Observer<Integer> dd = new Observer<Integer>() {
         @Override
-        public void onChanged(Integer integer) {
-            Log.i("###log", "retre" + count.getValue());
+        public void onChanged(Integer value) {
+            Log.i("###log", "retre" + value);
+            counterTv.setText(value + "");
         }
     };
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-//        count.setValue(count.getValue() + 1);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-
+                start = event.getX();
                 return true;
             case MotionEvent.ACTION_MOVE:
-
+                counterFl.setTranslationX(event.getX() - start);
                 return true;
             case MotionEvent.ACTION_UP:
+                if (counterFl.getTranslationX() > counterFl.getWidth() * 0.5) {
+                    if (getIsLtr()) {
+                        add();
+                    } else {
+                        subtract();
+                    }
+                } else if (counterFl.getTranslationX() < -(counterFl.getWidth() * 0.5)) {
+                    if (getIsLtr()) {
+                        subtract();
+                    } else {
+                        add();
+                    }
+                }
 
-                if (ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_LTR) {
-                    Integer i = count.getValue();
-                    i += 1;
-                    count.setValue(i);
-                } else {
-                    Integer i = count.getValue();
-                    i--;
-                    count.setValue(i);
+                if (counterFl.getTranslationX() != 0f) {
+                    counterFl.setTranslationX(0f);
                 }
                 return true;
-
-
             default:
                 return false;
         }
@@ -79,5 +92,19 @@ public class EasySlideTouch extends ConstraintLayout {
 //        return super.onTouchEvent(event);
     }
 
+    private void add() {
+        Integer i = count.getValue();
+        i += 1;
+        count.setValue(i);
+    }
 
+    private void subtract() {
+        Integer i = count.getValue();
+        i -= 1;
+        count.setValue(i);
+    }
+
+    private boolean getIsLtr () {
+        return ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_LTR;
+    }
 }
